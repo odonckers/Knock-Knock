b4f0684ad6621abdf4ee74fd558732a89a477977
Owen Donckers <odonckers@gmail.com>
2021-02-19 16:26:03 -0600

    Initial Commit

---
+++ Knock-Knock/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Knock-Knock
+//
+//  Created by Owen Donckers on 2/19/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
