242e7ce463b4829fe070b3afcf29f11292efbb04
Owen Donckers <odonckers@gmail.com>
2021-02-19 23:52:39 -0600

    Moved back to SwiftUI Life Cycle

---
+++ Knock-Knock/KnockKnockApp.swift
@@ -0,0 +1,23 @@
+//
+//  KnockKnockApp.swift
+//  Knock-Knock
+//
+//  Created by Owen Donckers on 2/19/21.
+//
+
+import SwiftUI
+
+@main
+struct KnockKnockApp: App {
+    let persistenceController = PersistenceController.shared
+    
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+        .commands {
+            SidebarCommands()
+        }
+    }
+}
